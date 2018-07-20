35377ca86b5e51221c9842ba2de29ed8a4fd2585
thanksLee <dmzone75@naver.com>
2018-07-20 23:31:57 +0900

    - initialize

---
+++ example/ex_common/src/main/java/com/vitamin/example/ex_common/App.java
@@ -0,0 +1,13 @@
+package com.vitamin.example.ex_common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
